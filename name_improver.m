f60cb98b0fb4c7d00a0012e036fff3d12a6bfc5a
HaukeHillebrandt <hauke.hillebrandt@gmail.com>
2014-11-20 13:52:40 +0100

    deleted_test function

--- name_improver.m
+++
@@ -1,7 +0,0 @@
-function [ Improved_name ] = important_function( your_name )
-%IMPORTANT_FUNCTION A function that makes your name better
-%   Detailed explanation goes here
-
-improved_name = your_name & 'banana'
-
-end
