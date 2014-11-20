2c16daa6239df198a5b16cf368721ac76953e7bf
HaukeHillebrandt <hauke.hillebrandt@gmail.com>
2014-11-20 13:39:53 +0100

    function added
    I have added a test function

---
+++ name_improver.m
@@ -0,0 +1,7 @@
+function [ Improved_name ] = important_function( your_name )
+%IMPORTANT_FUNCTION A function that makes your name better
+%   Detailed explanation goes here
+
+improved_name = your_name & 'banana'
+
+end
