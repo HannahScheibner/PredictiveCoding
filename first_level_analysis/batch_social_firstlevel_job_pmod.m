27beec301c41bee424be3c6e074ea9bde9b70801
HaukeHillebrandt <hauke.hillebrandt@gmail.com>
2014-11-29 01:47:42 +0100

    first level analysis added

--- first_level_analysis/batch_social_firstlevel_job_pmod.m
+++ first_level_analysis/batch_social_firstlevel_job_pmod.m
@@ -2,73 +2,92 @@ clc
 clear all
 
 % specify model name:
-model = 'smoothed_pmod'
+model = 'smoothed_pmod_test'
+comma = ','
+% number of scans
+number_of_scans = 672
 
 % for using spm_select to select data_dir
-[data_directory] = spm_select(1, 'dir' )
-% [files,dirs] = spm_select('ExtList',data_directory,'^sSub\d_Ses','inf')
+% [onset_directory] = spm_select(1, 'dir' )
+onset_directory = 'C:\Users\Peter\Documents\GitHub\PredictiveCoding'
 
+cd(onset_directory);
+load('onsets.mat')
+
+% [files,dirs] = spm_select('ExtList',data_directory,'^sSub\d_Ses','inf')
+% [data_directory] = spm_select(1, 'dir' )
+data_directory = 'C:\Users\Peter\Documents\MATLAB\PredictiveCoding\download\vim-1'
 cd(data_directory);
 dir_var = dir;
 
 
-
-for subject = 1:2
+for subject = 1
     
     % Create subject specific directories
     fmri_directory = sprintf('%s\\fmri_results\\%s\\subject%d', data_directory, model, subject)
-    mkdir(fmri_directory) 
+    mkdir(fmri_directory)
     
     matlabbatch{1}.spm.stats.fmri_spec.dir = {fmri_directory}
     matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'secs';
     matlabbatch{1}.spm.stats.fmri_spec.timing.RT = 1;
     matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t = 18;
     matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t0 = 9;
- 
-% Counter for 25 runs (in SPM all called sessions) 5 sessions * 5 runs for
-% each subject
-
- spm_session = 0;
-
+    
+    % Counter for 25 runs (in SPM all called sessions) 5 sessions * 5 runs for
+    % each subject
+    
+    spm_session = 0;
+    
     for session = 1:5
         
         for run = 1:5
             
-        spm_session = spm_session + 1
-
-       [run_files] = spm_select('FPListRec',data_directory,sprintf('sSub%d_Ses%d_Run%d_.*', subject, session, run));
-
-       matlabbatch{1,1}.spm.stats.fmri_spec.sess(spm_session,1).scans{scan,1} = run_files;
-     
-
-        pmod = 1;
-        
-        
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).name = 'All picture onsets';
-        
-        % here a file with all the onsets of all pictures must be specified
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).onset = events3(:,1);
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).duration = events3(1,2);
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).tmod = 0;
-        
-        % Here a file with all the corresponding Predictive coding values or filesizes must be specified
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).pmod(1).name = 'Filesize';
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).pmod(1).param = events3(:,3);
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).pmod(1).poly = 1;
-        
-        
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).cond(pmod).orth = 1;
-        
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).multi = {''};
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).regress = struct('name', {}, 'val', {});
-        
-        [movement_file] = spm_select('FPListRec',event_directory,'Movement_Regressors.txt')
-        
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).multi_reg = {movement_file};
-        matlabbatch{1}.spm.stats.fmri_spec.sess(session).hpf = 128;
-        
+            spm_session = spm_session + 1
+            
+            for scan = 1:number_of_scans
+                
+                session_scans = sprintf('%s\\Sub%d_Ses%d_Run%d_Trn.nii%s%d', data_directory, subject, session, run, comma, scan)
+                matlabbatch{1,1}.spm.stats.fmri_spec.sess(1,spm_session).scans{scan,1} = session_scans;
+                
+            end
+            
+            
+            
+            
+            
+            
+            %             [run_files] = spm_select('FPListRec',data_directory,sprintf('^Sub%d_Ses%d_Run%d_Trn.nii', subject, session, run), 1:672);
+            %
+            %             matlabbatch{1,1}.spm.stats.fmri_spec.sess(spm_session,1).scans{1,1} = run_files;
+            %
+            
+            pmod = 1;
+            
+            
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).name = 'All picture onsets';
+            
+            % here a file with all the onsets of all pictures must be specified
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).onset = [onsets.all{:,spm_session}]';
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).duration = 1;
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).tmod = 0;
+            
+            % Here a file with all the corresponding Predictive coding values or filesizes must be specified
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).pmod(1).name = 'Filesize';
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).pmod(1).param = [onsets.filesize{:,spm_session}]';
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).pmod(1).poly = 1;
+            
+            matlabbatch{1}.spm.stats.fmri_spec.sess(spm_session).cond(pmod).orth = 1;
+            %
+            %                         matlabbatch{1}.spm.stats.fmri_spec.sess(session).multi = {''};
+            %                         matlabbatch{1}.spm.stats.fmri_spec.sess(session).regress = struct('name', {}, 'val', {});
+            %
+            %                         [movement_file] = spm_select('FPListRec',event_directory,'Movement_Regressors.txt')
+            
+            %             matlabbatch{1}.spm.stats.fmri_spec.sess(session).multi_reg = {movement_file};
+            %             matlabbatch{1}.spm.stats.fmri_spec.sess(session).hpf = 128;
+            
+        end
     end
-    
     
     
     matlabbatch{1}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
@@ -95,5 +114,6 @@ for subject = 1:2
     spm('defaults', 'FMRI');
     spm_jobman('initcfg');
     spm_jobman('run',matlabbatch);
+    
     
 end
